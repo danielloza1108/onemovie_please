288b4c82ded928645a9f45d6e12be6f4dbeb2268
Daniel <danielloza1108@gmail.com>
2020-12-02 22:39:57 +0100

    fix

--- src/main/java/com/onemovieplease/onemovie_please/contollers/MainPage.java
+++ src/main/java/com/onemovieplease/onemovie_please/contollers/MainPage.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping("/")
 public class MainPage{
     @GetMapping("")
-    @ResponseBody
     public String mainPage() {
         return "mainPage";
     }
