5125067d49c1e108a39c4a35c45b09e2af0d75a6
Daniel <danielloza1108@gmail.com>
2020-12-02 22:27:52 +0100

    change

--- src/main/java/com/onemovieplease/onemovie_please/contollers/MainPage.java
+++ src/main/java/com/onemovieplease/onemovie_please/contollers/MainPage.java
@@ -2,15 +2,13 @@ package com.onemovieplease.onemovie_please.contollers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping(value = "/")
 public class MainPage {
     @GetMapping("")
-    public String mainPage(){
-        return "mainPage";
-    }
-    @GetMapping("/")
-    public String mainPage2(){
+    public String mainPage() {
         return "mainPage";
     }
 }
