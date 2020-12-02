17a4d2a384b1cf2b3c4e3a789c31c2b748b5043c
Daniel <danielloza1108@gmail.com>
2020-12-02 22:37:33 +0100

    try

--- src/main/java/com/onemovieplease/onemovie_please/contollers/MainPage.java
+++ src/main/java/com/onemovieplease/onemovie_please/contollers/MainPage.java
@@ -3,11 +3,13 @@ package com.onemovieplease.onemovie_please.contollers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/")
-public class MainPage {
+public class MainPage{
     @GetMapping("")
+    @ResponseBody
     public String mainPage() {
         return "mainPage";
     }
