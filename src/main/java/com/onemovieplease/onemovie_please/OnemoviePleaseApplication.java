2895afa6347ed5b6f6c90698402ae562463fcfa1
Daniel <danielloza1108@gmail.com>
2020-12-03 05:03:57 +0100

    try to fix

--- src/main/java/com/onemovieplease/onemovie_please/OnemoviePleaseApplication.java
+++ src/main/java/com/onemovieplease/onemovie_please/OnemoviePleaseApplication.java
@@ -3,12 +3,12 @@ package com.onemovieplease.onemovie_please;
 import org.bouncycastle.crypto.generators.BCrypt;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 import org.thymeleaf.templateresolver.ITemplateResolver;
-
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class OnemoviePleaseApplication {
 
